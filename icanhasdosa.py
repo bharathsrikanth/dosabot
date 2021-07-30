539fe8f12ced59bfc37871143f1e6d7496ee4e2e
bharathsrikanth <bharath@berbix.com>
2021-07-30 00:21:14 -0700

    Initial script to click on I Accept

---
+++ icanhasdosa.py
@@ -0,0 +1,22 @@
+from selenium import webdriver as wd 
+from selenium.common.exceptions import NoSuchElementException
+import chromedriver
+
+wd = wd.Chrome()
+wd.implicitly_wait(10)
+
+wd.get("https://suryadarshinicom.wixsite.com/sdhome")
+
+while(True):
+    try: 
+        accept_link = wd.find_element_by_link_text("I ACCEPT")
+        accept_link.click()
+        print('Clicked')
+        break
+    except NoSuchElementException:
+        print("Link Not Found")
+
+
+
+
+
